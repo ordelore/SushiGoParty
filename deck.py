a5444e3c64c5b96659952b6349f340b9360d5695
Lorenzo Orders <ordelore@uchicago.edu>
2019-09-25 12:40:38 -0500

    Updated deck mechanics

---
+++ deck.py
@@ -0,0 +1,81 @@
+import random
+
+nigiriArray = ["Egg Nigiri", "Salmon Nigiri", "Squid Nigiri"]
+rollsArray = ["Maki", "Temaki", "Uramaki"]
+appetizerArray = ["Tempura", "Sashimi", "Dumpling", "Eel", "Tofu", "Onigiri", "Edamame", "Miso Soup"]
+specialArray = ["Chopsticks", "Soy Sauce", "Tea", "Menu", "Spoon", "Special Order", "Takeout Box", "Wasabi"]
+dessertArray = ["Pudding", "Green Tea Ice Cream", "Fruit"]
+parentArray = [nigiriArray, rollsArray, appetizerArray, specialArray, dessertArray]
+
+numberOfNigiri = 12
+
+numberOfSushiRolls = 12
+numberOfAppetizers = 8
+numberOfSpecials = 3
+numberOfFirstDesserts = 5
+class Deck:
+
+    def __init__(self, rollIndex, appetizerIndices, specialIndices, dessertIndex):
+        if (self.resetDeck(rollIndex, appetizerIndices, specialIndices, dessertIndex) == 0):
+            return 0
+        
+    def dealCards(self, numberOfCards):
+        hand = []
+        for i in range(numberOfCards):
+            hand.append(self.deck.pop(0))
+        return hand
+
+    def returnCards(self, leftoverCards):
+        self.deck = self.deck + leftoverCards
+        
+
+    def resetDeck(self, rollIndex, appetizerIndices, specialIndices, dessertIndex):
+        if (len(appetizerIndices) != 3):
+            return 0
+        if (len(specialIndices) != 2):
+            return 0
+        
+        self.rollType = rollIndex
+        
+        self.appetizerOne = appetizerIndices[0]
+        self.appetizerTwo = appetizerIndices[1]
+        self.appetizerThree = appetizerIndices[2]
+        
+        self.specialOne = specialIndices[0]
+        self.specialTwo = specialIndices[1]
+        
+        self.dessertType = dessertIndex
+        
+        self.deck = []
+        
+        #four egg nigiri
+        #five salmon nigiri
+        #three squid nigiri
+        self.deck = self.deck + [0,0,0,0,1,1,1,1,1,2,2,2]
+        
+        for i in range(numberOfSushiRolls):
+            self.deck.append(10 + rollIndex)
+        
+        for i in range(numberOfAppetizers):
+            self.deck.append(20 + self.appetizerOne)
+            self.deck.append(20 + self.appetizerTwo)
+            self.deck.append(20 + self.appetizerThree)
+        
+        for i in range(numberOfSpecials):
+            self.deck.append(30 + self.specialOne)
+            self.deck.append(30 + self.specialTwo)
+        
+        for i in range(numberOfFirstDesserts):
+            self.deck.append(40 + self.dessertType)
+        
+        random.shuffle(self.deck)
+def printHand(hand):
+    string = ""
+    for card in hand:
+        string = string + parentArray[get_cardType(card)][get_cardTypeIndex(card)] + ", "
+    print(string)
+def get_cardType(value):
+    return int(value / 10)
+    
+def get_cardTypeIndex(value):
+    return value % 10
---
+++ main.py
@@ -0,0 +1,6 @@
+import deck
+
+currentDeck = deck.Deck(1, [0,1,7], [7, 2], 1)
+deck.printHand(currentDeck.dealCards(10))
+deck.printHand(currentDeck.dealCards(10))
+deck.printHand(currentDeck.dealCards(10))
